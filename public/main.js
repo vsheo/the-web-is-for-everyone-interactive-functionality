2726a9079a2df9c6feec3969331b645a941c659a
vsheo <vireshsheoratan@gmail.com>
2025-03-21 10:44:35 +0100

    reset loading circle #19

--- public/main.js
+++ public/main.js
@@ -3,10 +3,18 @@
 const page = document.querySelector("main");
 const anchorTags = document.querySelectorAll("a");
 const loading = document.querySelector(".loading-circle");
+const overlay = document.querySelector(".overlay");
 
-// op elke link is er een event istener
-anchorTags.forEach((link) => {
-    link.addEventListener("click", () => {
-        loading.classList.remove("hidden");
+// als de pagina ingeladen is, gaat de loading circle weg
+document.addEventListener("DOMContentLoaded", (event) => {
+    loading.classList.add("hidden");
+
+    // op elke link is er een event istener
+    anchorTags.forEach((link) => {
+        // bij een klik komt de loading circle op beeld
+        link.addEventListener("click", () => {
+            loading.classList.remove("hidden");
+            overlay.classList.remove("hidden");
+        });
     });
 });
