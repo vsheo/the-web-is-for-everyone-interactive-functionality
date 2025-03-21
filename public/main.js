6f574955d773ca2bfba771deb103505014687bd6
vsheo <vireshsheoratan@gmail.com>
2025-03-21 10:46:19 +0100

    extra code weg #19

--- public/main.js
+++ public/main.js
@@ -5,16 +5,12 @@ const anchorTags = document.querySelectorAll("a");
 const loading = document.querySelector(".loading-circle");
 const overlay = document.querySelector(".overlay");
 
-// als de pagina ingeladen is, gaat de loading circle weg
-document.addEventListener("DOMContentLoaded", (event) => {
-    loading.classList.add("hidden");
 
-    // op elke link is er een event istener
-    anchorTags.forEach((link) => {
-        // bij een klik komt de loading circle op beeld
-        link.addEventListener("click", () => {
-            loading.classList.remove("hidden");
-            overlay.classList.remove("hidden");
-        });
+// op elke link is er een event istener
+anchorTags.forEach((link) => {
+    // bij een klik komt de loading circle op beeld
+    link.addEventListener("click", () => {
+        loading.classList.remove("hidden");
+        overlay.classList.remove("hidden");
     });
 });
