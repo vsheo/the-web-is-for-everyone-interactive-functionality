04ad6de9f99c9b09ec2687255620120f4bd7a2f2
vsheo <vireshsheoratan@gmail.com>
2025-03-23 22:07:43 +0100

    loading circle back button #21 #19
    de loading bar blijf nogsteeds op beeld als de back button geklikt wordt

--- public/main.js
+++ public/main.js
@@ -1,27 +1,37 @@
 // loading bar als een link geklikt wordt
-const page = document.querySelector("main");
+const loading = document.querySelector(".loading-overlay");
 const anchorTags = document.querySelectorAll("a");
-const loading = document.querySelector(".loading-circle");
-const overlay = document.querySelector(".overlay");
-const loadingTekst = document.querySelector(".loading-text");
 
 
-// op elke link is er een event istener
-anchorTags.forEach((link) => {
-    // bij een klik komt de loading circle op beeld
-    link.addEventListener("click", () => {
-        loading.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-        loadingTekst.classList.remove("hidden");
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    if (loading.classList.contains("hidden")) {
+        // op elke link is er een event istener
+        anchorTags.forEach((link) => {
+            // bij een klik komt de loading circle op beeld
+            link.addEventListener("click", () => {
+                loading.classList.remove("hidden");
+            });
+        });
+    }
+    else {
+        loading.classList.add("hidden");
+    }
 });
+
+// window.addEventListener("popstate", () => {
+//     loading.classList.add("hidden");
+// });
+
+// window.addEventListener('load', () => {
+//     loading.classList.add("hidden");
+// });
 
 
 // back button
 // deze button brengt de gebruiker naar de vorige pagina
-
 const backButton = document.querySelector(".button-back");
+
 backButton.addEventListener("click", () => {
     // als de button geklikt wordt ga naar de vorige pagina
-    window.history.back();
+    window.history.back()
 });
